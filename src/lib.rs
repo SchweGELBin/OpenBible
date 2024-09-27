e004bb100f1c9c674e98ebedb65114d0499e6f8b
SchweGELBin <abramjannikmichael06@gmail.com>
2024-09-27 23:41:51 +0200

    feat: implement basic functionality

--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,42 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
 
+use slint::{self, android};
+
 #[derive(serde::Deserialize)]
 struct Verse {
     verse: usize,
     text: String,
 }
 
+const DATA_DIR: &str = "/storage/emulated/0/Android/data/com.schwegelbin.openbible/files";
+
 // Android Main Function
 #[no_mangle]
-fn android_main(app: slint::android::AndroidApp) {
-    slint::android::init(app).unwrap();
-
-    // ... rest of your code ...
+fn android_main(app: android::AndroidApp) {
+    android::init(app).unwrap();
     slint::slint! {
         export component MainWindow inherits Window {
-            Text { text: "Hello World"; }
+            in-out property<string> text;
+            Text { text: text; }
         }
     }
-    MainWindow::new().unwrap().run().unwrap();
+    let ui = MainWindow::new().unwrap();
+
+    save_index();
+    download_translation("schlachter");
+    let update_available = check_update("schlachter");
+
+    ui.set_text(get_chapter("schlachter", 5, 6).unwrap().into());
+
+    ui.run().unwrap();
 }
 
 // Saves an index of all translations
 fn save_index() -> Result<(), Box<dyn Error>> {
     const TURL: &str = "https://api.getbible.net/v2/translations.json";
     let translations = reqwest::blocking::get(TURL)?.bytes()?;
-    fs::write("translations.json", &translations)?;
+    fs::write(format!("{}/translations.json", DATA_DIR), &translations)?;
     Ok(())
 }
 
@@ -35,15 +46,15 @@ fn download_translation(abbrev: &str) -> Result<(), Box<dyn Error>> {
     let turl = format!("https://api.getbible.net/v2/{}.json", &abbrev);
     let translation = reqwest::blocking::get(turl)?.bytes()?;
     let checksum = get_latest_checksum(&abbrev)?;
-    fs::write(format!("{}-checksum.json", &abbrev), &checksum)?;
-    fs::write(format!("{}.json", &abbrev), &translation)?;
+    fs::write(format!("{}/{}-checksum.json", DATA_DIR, &abbrev), &checksum)?;
+    fs::write(format!("{}/{}.json", DATA_DIR, &abbrev), &translation)?;
     Ok(())
 }
 
 // Return a bool, wether the online version has a different checksum
 fn check_update(abbrev: &str) -> Result<bool, Box<dyn Error>> {
     let latest = get_latest_checksum(&abbrev)?;
-    let current = fs::read_to_string(format!("{}-checksum.json", &abbrev))?;
+    let current = fs::read_to_string(format!("{}/{}-checksum.json", DATA_DIR, &abbrev))?;
     Ok(latest != current)
 }
 
@@ -54,15 +65,16 @@ fn get_latest_checksum(abbrev: &str) -> Result<String, Box<dyn Error>> {
     Ok(checksum[abbrev].clone())
 }
 
-// Print every verse of a chosen chapter of a chosen book of a chosen translation
-fn print_chapter(abbrev: &str, book: usize, chapter: usize) -> Result<(), Box<dyn Error>> {
-    let file: String = fs::read_to_string(format!("{}.json", &abbrev))?;
+// Returns a String of every verse of a chosen chapter of a chosen book of a chosen translation
+fn get_chapter(abbrev: &str, book: usize, chapter: usize) -> Result<String, Box<dyn Error>> {
+    let file: String = fs::read_to_string(format!("{}/{}.json", DATA_DIR, &abbrev))?;
     let json: serde_json::Value = serde_json::from_str(&file)?;
+    let mut chapter_string = String::new();
     let verses: Vec<Verse> =
         serde_json::from_value(json["books"][book]["chapters"][chapter]["verses"].clone())?;
     println!("{}", json["books"][book]["chapters"][chapter]["name"]);
     for verse in verses {
-        println!("{}\t{}", verse.verse, verse.text);
+        chapter_string.push_str(format!("{}\t{}\n", verse.verse, verse.text).as_str());
     }
-    Ok(())
+    Ok(chapter_string)
 }
